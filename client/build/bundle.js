555a6caa430d7c60800f64dece5805e616670fce
cbacon <bacon.chris@gmail.com>
2017-02-05 21:08:20 +0000

    fixed some typos, added css styling to force planets to be off page, and split ui into multiple files

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -93,61 +93,10 @@ module.exports = planet;
 /* 1 */
 /***/ (function(module, exports, __webpack_require__) {
 
-var Planets = __webpack_require__(2);
-var Planet = __webpack_require__(0)
+var imgUI = __webpack_require__(4);
 
 var UI = function() {
-    this.planets = new Planets;
-
-    this.planets.all(function(result) {
-        this.render(result);
-    }.bind(this))
-}
-
-UI.prototype = {
-
-    createImage: function(url) {
-        var img = document.createElement('img');
-        img.className = "planet"
-        img.src = url;
-        return img;
-    },
-
-    clickImage: function(img, planet) {
-        var body = document.querySelector('body')
-        var div = document.querySelector('#popupBox')
-        var span = document.getElementsByClassName('close')[0];
-        var p = document.querySelector('p')
-      
-        img.onclick = function() {
-            div.style.display = 'block';
-            p.innerText = planet.name + "\n" + planet.overview + "\n" + "Number of Moons: " + planet.moonValue + "\n" + "Distance from the sun: " + planet.distanceToSun + "AU";
-        }
-
-        span.onclick = function() {
-            div.style.display = 'none';
-        }
-
-        window.onclick = function(event) {
-            if(event.target == div) {
-                div.style.display = 'none';
-            }
-        }
-         
-    },
-
-    render: function(planets) {
-        var main = document.querySelector('main');
-        
-        
-        for (var planet of planets) {
-            var img = this.createImage(planet.getImage());
-            this.clickImage(img, planet);
-            main.appendChild(img);
-        }
-
-    }
-
+    new imgUI();
 }
 
 module.exports = UI;
@@ -212,6 +161,69 @@ var app = function(){
 }
 
 window.onload = app;
+
+/***/ }),
+/* 4 */
+/***/ (function(module, exports, __webpack_require__) {
+
+var Planets = __webpack_require__(2);
+var Planet = __webpack_require__(0);
+
+var ImgUI = function() {
+    this.planets = new Planets;
+
+    this.planets.all(function(result) {
+        this.render(result);
+    }.bind(this))
+}
+
+ImgUI.prototype = {
+
+    createImage: function(url) {
+        var img = document.createElement('img');
+        img.className = "planet"
+        img.src = url;
+        return img;
+    },
+
+    clickImage: function(img, planet) {
+        var body = document.querySelector('body')
+        var div = document.querySelector('#popupBox')
+        var span = document.getElementsByClassName('close')[0];
+        var p = document.querySelector('p')
+      
+        img.onclick = function() {
+            div.style.display = 'block';
+            p.innerText = planet.name + "\n" + planet.overview + "\n" + "Number of Moons: " + planet.moonValue + "\n" + "Distance from the sun: " + planet.distanceToSun + "AU";
+        }
+
+        span.onclick = function() {
+            div.style.display = 'none';
+        }
+
+        window.onclick = function(event) {
+            if(event.target == div) {
+                div.style.display = 'none';
+            }
+        }
+         
+    },
+
+    render: function(planets) {
+        var main = document.querySelector('main');
+        
+        
+        for (var planet of planets) {
+            var img = this.createImage(planet.getImage());
+            this.clickImage(img, planet);
+            main.appendChild(img);
+        }
+
+    }
+
+}
+
+module.exports = ImgUI;
 
 /***/ })
 /******/ ]);
--- client/src/views/ui.js
+++ client/src/views/ui.js
@@ -1,58 +1,7 @@
-var Planets = require('../models/planets');
-var Planet = require('../models/planet')
+var imgUI = require('./imgui');
 
 var UI = function() {
-    this.planets = new Planets;
-
-    this.planets.all(function(result) {
-        this.render(result);
-    }.bind(this))
-}
-
-UI.prototype = {
-
-    createImage: function(url) {
-        var img = document.createElement('img');
-        img.className = "planet"
-        img.src = url;
-        return img;
-    },
-
-    clickImage: function(img, planet) {
-        var body = document.querySelector('body')
-        var div = document.querySelector('#popupBox')
-        var span = document.getElementsByClassName('close')[0];
-        var p = document.querySelector('p')
-      
-        img.onclick = function() {
-            div.style.display = 'block';
-            p.innerText = planet.name + "\n" + planet.overview + "\n" + "Number of Moons: " + planet.moonValue + "\n" + "Distance from the sun: " + planet.distanceToSun + "AU";
-        }
-
-        span.onclick = function() {
-            div.style.display = 'none';
-        }
-
-        window.onclick = function(event) {
-            if(event.target == div) {
-                div.style.display = 'none';
-            }
-        }
-         
-    },
-
-    render: function(planets) {
-        var main = document.querySelector('main');
-        
-        
-        for (var planet of planets) {
-            var img = this.createImage(planet.getImage());
-            this.clickImage(img, planet);
-            main.appendChild(img);
-        }
-
-    }
-
+    new imgUI();
 }
 
 module.exports = UI;
