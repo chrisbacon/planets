e6581ea392c54cdf0dfd7924c5c22a9d13520e2b
Jane <jane-cargill@hotmail.co.uk>
2017-02-05 19:03:46 +0000

    click on image and popup box details planet info

--- client/src/views/ui.js
+++ client/src/views/ui.js
@@ -16,14 +16,39 @@ UI.prototype = {
         img.src = url;
         return img;
     },
+    clickImage: function(img, planet) {
+        var body = document.querySelector('body')
+        var div = document.querySelector('#popupBox')
+      var span = document.getElementsByClassName('close')[0];
+      var p = document.querySelector('p')
+      
+           img.onclick = function() {
+            div.style.display = 'block';
+            p.innerText = planet.name + "\n" + planet.overview + "\n" + "Number of Moons: " + planet.moonValue + "\n" + "Distane from the sun: " + planet.distanceToSun + "AU";
+           }
+           
+           span.onclick = function() {
+            div.style.display = 'none';
+
+           }
+           window.onclick = function(event) {
+            if(event.target == div) {
+            div.style.display = 'none';
+            }
+           }
+         
+       },
 
     render: function(planets) {
         var main = document.querySelector('main');
+        
+        
         for (var planet of planets) {
-            console.log(planet.getImage())
             var img = this.createImage(planet.getImage());
+            this.clickImage(img, planet);
             main.appendChild(img);
         }
+
     }
 
 }
