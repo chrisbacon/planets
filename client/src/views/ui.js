c38e5dc94aad5d00333d13ddc85542d20133337c
cbacon <bacon.chris@gmail.com>
2017-02-05 20:37:41 +0000

    started navbar

--- client/src/views/ui.js
+++ client/src/views/ui.js
@@ -13,31 +13,33 @@ UI.prototype = {
 
     createImage: function(url) {
         var img = document.createElement('img');
+        img.className = "planet"
         img.src = url;
         return img;
     },
+
     clickImage: function(img, planet) {
         var body = document.querySelector('body')
         var div = document.querySelector('#popupBox')
-      var span = document.getElementsByClassName('close')[0];
-      var p = document.querySelector('p')
+        var span = document.getElementsByClassName('close')[0];
+        var p = document.querySelector('p')
       
-           img.onclick = function() {
+        img.onclick = function() {
             div.style.display = 'block';
-            p.innerText = planet.name + "\n" + planet.overview + "\n" + "Number of Moons: " + planet.moonValue + "\n" + "Distane from the sun: " + planet.distanceToSun + "AU";
-           }
-           
-           span.onclick = function() {
-            div.style.display = 'none';
+            p.innerText = planet.name + "\n" + planet.overview + "\n" + "Number of Moons: " + planet.moonValue + "\n" + "Distance from the sun: " + planet.distanceToSun + "AU";
+        }
 
-           }
-           window.onclick = function(event) {
-            if(event.target == div) {
+        span.onclick = function() {
             div.style.display = 'none';
+        }
+
+        window.onclick = function(event) {
+            if(event.target == div) {
+                div.style.display = 'none';
             }
-           }
+        }
          
-       },
+    },
 
     render: function(planets) {
         var main = document.querySelector('main');
