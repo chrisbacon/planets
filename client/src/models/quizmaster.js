0ba15eccd14a119c3ffc166198f932c981423d6e
patrick13111993 <keriakospapadoopalus@gmail.com>
2017-02-08 13:09:46 +0000

    message fix

--- client/src/models/quizmaster.js
+++ client/src/models/quizmaster.js
@@ -35,9 +35,9 @@ QuizMaster.prototype = {
     var wellDone = "";
     if(this.score < 5) {
       wellDone = "Keep practicing!";
-    } else if (4 < this.score < 7) {
+    } else if (7 > this.score > 4) {
       wellDone = "Well done!";
-    } else if (6 < this.score < 10) {
+    } else if (10 > this.score > 6) {
       wellDone = "Very impressive!" ;
     } else if (this.score === 10) {
       wellDone = "Congratulations, you are a space expert!"
